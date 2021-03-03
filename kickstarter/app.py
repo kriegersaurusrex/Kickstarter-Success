de9da104af821c6562bf456eb8c96d79fbaeab80
afroRoboticist <afroroboticist@gmail.com>
2021-03-03 09:49:37 -0500

    Predictions updated

--- kickstarter/app.py
+++ kickstarter/app.py
@@ -3,12 +3,19 @@ import numpy as np
 import pickle
 from flask import Flask, redirect, url_for, render_template
 import os
+from joblib import dump, load
 from .forms import KickStarterForm
-from sklearn.impute import SimpleImputer
-from sklearn.metrics import accuracy_score, mean_squared_error
 from sklearn.model_selection import train_test_split
-from sklearn.pipeline import make_pipeline, Pipeline
+from category_encoders import OneHotEncoder, OrdinalEncoder
+from sklearn.ensemble import RandomForestClassifier
 from sklearn.ensemble import RandomForestRegressor
+from sklearn.impute import SimpleImputer
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import accuracy_score, plot_confusion_matrix, classification_report
+from sklearn.model_selection import train_test_split
+from sklearn.pipeline import make_pipeline
+from sklearn.preprocessing import StandardScaler
+import seaborn as sns
 from dotenv import load_dotenv
 
 
@@ -31,48 +38,118 @@ def create_app():
 
 	@app.route('/run_model', methods=['GET','POST'])
 	def run_model():
+		proceed_flag = True
 		form = KickStarterForm()
-		if form.validate_on_submit():
-			print('Validating form data')
-			name = form.kickstarter_name.data
-			ks_id = form.kickstarter_id.data
-			ks_blurb = form.kickstarter_blurb.data
-			ks_created = form.kickstarter_created.data
-			ks_lunched = form.kickstarter_launched.data
-			ks_deadline = form.kickstarter_deadline.data
-			ks_staffpick = form.kickstarter_staffpick.data
-			ks_locationtype = form.kickstarter_locationtype.data
-			ks_locationstate = form.kickstarter_locationstate.data
-			ks_country = form.kickstarter_country.data
-			ks_currency = form.kickstarter_currency.data
-			ks_creatorid = form.kickstarter_creatorid.data
-			ks_usdgoal = form.kickstarter_usdgoal.data
-			ks_usdpledge = form.kickstarter_usdpledge.data
-			ks_creatorid = form.kickstarter_creatorid.data
-			ks_creatorname = form.kickstarter_creatorname.data
-			ks_locationid = form.kickstarter_locationid.data
-			ks_spotlight = form.kickstarter_spotlight.data
-			ks_categoryname = form.kickstarter_categoryname.data
-			ks_categoryslug = form.kickstarter_categoryslug.data
-			ks_categoryparentid = form.kickstarter_categoryparentid.data
-			ks_categoryposition = form.kickstarter_categoryposition.data
+		print('Form Exectuted... Now Validating')
+		print('Form Validation: ', form.validate())
+		for fieldname, value in form.data.items():
+			print(fieldname, value)
+			if value == None:
+				proceed_flag = False
+			
+		print('Form Validation Status: ',form.validate())
 
+		if proceed_flag:
+			data_input = prepare_input_data(form)
+			result = make_prediction(data_input)
 			# This is where you enter all of this data into your model    #
 			# Note, still testing entry data to see what output types are #
-			return redirect(url_for('display_results'))
-
+			return render_template('display_success.html', form=form, result=result)
+		
 		return render_template('run_model.html', form=form)
 
 
 
-	@app.route('/display_results')
-	def display_results():
-		return render_template('home.html')
+	# @app.route('/display_results')
+	# def display_results():
+	# 	return render_template('display_success.html')
 
 
 	@app.route('/about')
 	def about_page():
 		return render_template('about.html')
+
+
+	def prepare_input_data(form):
+		
+
+		dict_keys = ['id', 'name', 'blurb', 'currency', 'created_at', 'launched_at',
+				'deadline', 'usd_goal', 'country_displayable_name', 'slug',
+				'creator_name', 'category_id', 'category_name', 'category_slug',
+				'category_position', 'category_parent_id', 'location_displayable_name',
+				'location_state', 'location_type', 'location_expanded_country',
+				'days_for_project']
+		name = form.kickstarter_name.data
+		ks_id = form.kickstarter_id.data
+		ks_blurb = form.kickstarter_blurb.data
+		ks_created = form.kickstarter_created.data
+		ks_launched = form.kickstarter_launched.data
+		ks_deadline = form.kickstarter_deadline.data
+		ks_staffpick = form.kickstarter_staffpick.data
+		ks_locationtype = form.kickstarter_locationtype.data
+		ks_locationstate = form.kickstarter_locationstate.data
+		ks_country = form.kickstarter_country.data
+		ks_countrydisp = form.kickstarter_countrydisplayable.data
+		ks_currency = form.kickstarter_currency.data
+		ks_creatorid = form.kickstarter_creatorid.data
+		ks_usdgoal = form.kickstarter_usdgoal.data
+		ks_usdpledge = form.kickstarter_usdpledge.data
+		ks_creatorid = form.kickstarter_creatorid.data
+		ks_creatorname = form.kickstarter_creatorname.data
+		ks_locationid = form.kickstarter_locationid.data
+		ks_locationdisplayable = form.kickstarter_locationdisplayable.data
+		ks_spotlight = form.kickstarter_spotlight.data
+		ks_categoryid = form.kickstarter_categoryid.data
+		ks_categoryname = form.kickstarter_categoryname.data
+		ks_categoryslug = form.kickstarter_categoryslug.data
+		ks_slug = form.kickstarter_slug.data
+		ks_categoryparentid = form.kickstarter_categoryparentid.data
+		ks_categoryposition = form.kickstarter_categoryposition.data
+
+		input_dict = {'id': [ks_id],
+					  'name': [name],
+					  'blurb': [ks_blurb],
+					  'currency': [ks_currency],
+					  'created_at': [ks_created],
+					  'launched_at': [ks_launched],
+					  'deadline': [ks_deadline],
+					  'usd_goal': [ks_usdgoal],
+					  'country_displayable_name': [ks_countrydisp],
+					  'slug': [ks_slug],
+					  'creator_name': [ks_creatorname],
+					  'category_id': [ks_categoryid],
+					  'category_name': [ks_categoryname],
+					  'category_slug': [ks_categoryslug],
+					  'category_position': [ks_categoryposition],
+					  'category_parent_id': [ks_categoryparentid],
+					  'location_displayable_name': [ks_locationdisplayable],
+					  'location_state': [ks_locationstate],
+					  'location_type': [ks_locationtype],
+					  'location_expanded_country': [ks_country]
+					   }
+		print('Input Dict:',input_dict)
+		df = pd.DataFrame.from_dict(input_dict)
+		df['created_at2'] = pd.to_datetime(df['created_at'], format='%m/%d/%Y')
+		df['launched_at2'] = pd.to_datetime(df['launched_at'], format='%m/%d/%Y')
+		df['deadline2'] = pd.to_datetime(df['deadline'], format='%m/%d/%Y')
+		#New columns
+		df['days_for_project'] = df['deadline2']-df['launched_at2']
+		df['days_for_project'] = pd.to_numeric(df['days_for_project'].dt.days, downcast='integer')
+		df.drop(columns=['created_at2', 'launched_at2', 'deadline2'], inplace=True)
+		print(df[['launched_at','deadline','created_at']])
+		print(df.head())
+		return df
+
+	def make_prediction(X_data):
+		predictions= {0: 'Fail', 1: 'Succeed'}
+		model = load('./kickstarter/static/kickstarter_model.sav')
+		prediction = model.predict(X_data)
+		print('Your Prediction is: ',prediction)
+		return predictions[prediction[0]]
+
+
+
+
 
 
 	return app
--- kickstarter/forms.py
+++ kickstarter/forms.py
@@ -2,7 +2,7 @@ import pandas as pd
 from flask_wtf import FlaskForm
 from wtforms import IntegerField, SubmitField,\
 				StringField, SelectField, DecimalField,\
-				DateTimeField
+				DateField
 from wtforms.validators import DataRequired, ValidationError
 import os
 import pickle
@@ -27,6 +27,13 @@ LOCATION_COUNTRY = ['United States', 'United Kingdom', 'Canada', 'New Zealand',
        'Malaysia', 'Lithuania', 'Belize', 'Belarus', 'Ukraine',
        'Portugal', 'Chile', 'Philippines', 'South Africa', 'Serbia',
        'Estonia', 'Costa Rica']
+
+COUNTRY_DISPLAYABLE_NAME= ['United States', 'United Kingdom', 'Canada', 'New Zealand',
+       'Germany', 'the Netherlands', 'Australia', 'Italy', 'Switzerland',
+       'Mexico', 'France', 'Denmark', 'Singapore', 'Hong Kong', 'Norway',
+       'Ireland', 'Spain', 'Japan', 'Sweden', 'Belgium', 'Austria',
+       'Luxembourg', 'Poland', 'Greece', 'Slovenia']
+
 CURRENCY = ['USD', 'GBP', 'CAD', 'NZD', 'EUR', 'AUD', 'CHF', 'MXN', 'DKK',
        'SGD', 'HKD', 'NOK', 'JPY', 'SEK']
 
@@ -50,43 +57,49 @@ with open('./kickstarter/static/category_name.pickle', 'rb') as handle:
     CATEGORY_NAME = pickle.load(handle)
 
 
+with open('./kickstarter/static/slug.pickle', 'rb') as handle:
+    SLUG = pickle.load(handle)
+
+
+with open('./kickstarter/static/category_id.pickle', 'rb') as handle:
+    CAT_ID = pickle.load(handle)
+
+with open('./kickstarter/static/location_displayable.pickle', 'rb') as handle:
+    LOC_DISP = pickle.load(handle)
 
 # KickStarter info form class definition
 class KickStarterForm(FlaskForm):
 	kickstarter_name = StringField(
-		label='Name of Kickstarter Campaign', 
-		validators=[DataRequired()])
+		label='Name of Kickstarter Campaign')
 	kickstarter_id = StringField(label='Campaign ID')
 	kickstarter_blurb = StringField(label='Blurb')
-	kickstarter_created = DateTimeField(label='Created At')
-	kickstarter_launched = DateTimeField(label='Launched At')
-	kickstarter_deadline = DateTimeField(label='Deadline')
+	kickstarter_created = StringField(label='Created At', render_kw={"placeholder": "mm/dd/YY"})
+	kickstarter_launched = StringField(label='Launched At', render_kw={"placeholder": "mm/dd/YY"})
+	kickstarter_deadline = StringField(label='Deadline', render_kw={"placeholder": "mm/dd/YY"})
 	kickstarter_staffpick = SelectField(label='Staff Pick', choices=BOOLEAN)
-	
+	kickstarter_slug = SelectField(label='Slug', choices=SLUG)
 	kickstarter_locationtype= SelectField(label='Location Type', choices=LOCATION_TYPE)
 	kickstarter_locationstate = SelectField(label='Location State', choices=LOCATION_STATE)
 	kickstarter_country= SelectField(label='Country', choices=LOCATION_COUNTRY)
+	kickstarter_locationdisplayable = SelectField(label='location displayable name',
+		choices=LOC_DISP)
+	kickstarter_countrydisplayable = SelectField(label='Country Displayable Name', choices=COUNTRY_DISPLAYABLE_NAME)
 	kickstarter_currency= SelectField(label='Currency', choices=CURRENCY)
 	kickstarter_creatorid = StringField(label='Creator ID')
 	kickstarter_usdgoal = DecimalField(
-		label='USD Goal  ', 
-		validators=[DataRequired(), validate_currency], render_kw={"placeholder": "$00.00"})
+		label='USD Goal  ', render_kw={"placeholder": "$00.00"})
 	kickstarter_usdpledge = DecimalField(
-		label='USD Pledge  ', 
-		validators=[DataRequired(), validate_currency], render_kw={"placeholder": "$00.00"})
+		label='USD Pledge  ',render_kw={"placeholder": "$00.00"})
 	kickstarter_creatorid = StringField(label='Creator ID')
 	kickstarter_creatorname = StringField(label='Creator name')
 	kickstarter_locationid = StringField(label='Location ID')
 	kickstarter_spotlight = SelectField(label='Spot Light', choices=BOOLEAN)
+	kickstarter_categoryid = SelectField(label='Category ID', choices=CAT_ID)
 	kickstarter_categoryname = SelectField(label='Category Name', choices=CATEGORY_NAME)
 	kickstarter_categoryslug = SelectField(label='Category Slug', choices=CATEGORY_SLUG)
 	kickstarter_categoryparentid = SelectField(label='Category ParentID', choices=sorted(CATEGORY_PARENTID))
 	kickstarter_categoryposition = SelectField(label='Category Position', choices=sorted(CATEGORY_POSITION))
 	submit = SubmitField('SUBMIT')
-
-
-
-
 
 
    
